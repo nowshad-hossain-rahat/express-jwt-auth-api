05f1527125a6da24ac4bcca8678177ea43ca5baf
nowshad-hossain-rahat <nowshad.hossain.rahat@gmail.com>
2022-07-11 09:48:45 +0600

    removed test files

--- test.js
+++
@@ -1,7 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-let pass = 'Abcd./1234/';
-let hash = CryptoJS.HmacSHA256(pass, '#SK.(NHR)');
-let hashStr = hash.toString(CryptoJS.enc.Base64);
-console.log(hashStr)
-
